300cc7cb1b170b22538a32e2457a1ecae54e3087
Hussein Karaky <karakyhussein@outlook.com>
2021-04-21 23:43:59 +0300

    Initialzing BLL

---
+++ BLL/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POS_Solutions.BLL
+{
+	public class App
+	{
+		public App()
+		{
+		}
+	}
+
+}
---
+++ BLL/User.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace POS_Solutions.BLL
+{
+    public class User
+    {
+    }
+}
